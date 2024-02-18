0097c78abbd5d87c56ddb2128a79000fb23ac149
Maran <www.sent2manimaran@gmail.com>
2024-02-18 22:22:36 +0530

    sum of digits is done in the recursion

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -1,9 +1,6 @@
-function sumOfDigit(num, sum=0) {
-    if(num <= 0) return sum;
-    let last = num%10;
-    num = Math.floor(num/10);
-    let ans = sumOfDigit(num, last)
-    return sum + ans;
+function sumOfDigit(num) {
+    if(num <= 0) return 0;
+    return num%10 + sumOfDigit(Math.floor(num/10));
 }
 
 console.log(sumOfDigit(1342));
